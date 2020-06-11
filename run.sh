7df1a969c268ae52a1e524546bf03fc70abcc6e4
Leeingnyo <dyd1928@naver.com>
2020-06-12 04:21:32 +0900

    fix tool scripts

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 set -e
 #  --device gpu/1080
-n=`ARGS="../common/examples/input_1.rgb myoutput.rgb" /usr/bin/make run | tail -n 2 | head -n 1 | cut -d ' ' -f 2`
+if [ $# == 1 ]; then
+  n=$1
+else
+  n=1
+fi
+n=`ARGS="../common/examples/input_${n}.rgb myoutput.rgb" /usr/bin/make run | tail -n 2 | head -n 1 | cut -d ' ' -f 2`
 echo $n
 sleep 2
 watch -n 1 cat "task_$n.stdout"
--- valid.sh
+++ valid.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ $# == 1 ]; then
+  n=$1
+else
+  n=1
+fi
 set -e
-../common/diffrgb myoutput.rgb ../common/examples/output_1.rgb
+../common/diffrgb myoutput.rgb ../common/examples/output_${n}.rgb
